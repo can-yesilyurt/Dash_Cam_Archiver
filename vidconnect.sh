59db607041d39ceb96dd9d72c05cbd6fb3573d0e
Can Yesilyurt <can--yesilyurt@hotmail.com>
2023-03-26 02:34:08 +0300

    Add files via upload

---
+++ vidconnect.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+ls $PWD/*.MP4 > files
+
+for i in {1..$(wc files | cut -d " " -f 6)}; do mv $(cat ./files | head -$i | tail -1) ./tmp.MP4; ffmpeg -i  ./tmp.MP4 -vf scale=680:-1 $(cat ./files | head -$i | tail -1); done
+
+for i in {1..$(wc files | cut -d " " -f 6)}; do echo "file $(cat ./files | head -$i | tail -1)" >> ./vidlist.txt; done
+
+ffmpeg -f concat -safe 0 -i vidlist.txt -c copy output.mp4
+
+echo "\n done \n"
+
+exit 0
