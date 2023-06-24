481e925488ef5d06a80db50944dc5d01ba6464c1
Can Yesilyurt <can--yesilyurt@hotmail.com>
2023-06-25 01:11:38 +0300

    Update vidconnect.sh

--- vidconnect.sh
+++ vidconnect.sh
@@ -2,9 +2,9 @@
 
 ls $PWD/*.MP4 > files
 
-for i in {1..$(wc files | cut -d " " -f 6)}; do mv $(cat ./files | head -$i | tail -1) ./tmp.MP4; ffmpeg -i  ./tmp.MP4 -vf scale=680:-1 $(cat ./files | head -$i | tail -1); done
+for i in {1..$(wc files | cut -d " " -f 7)}; do mv $(cat ./files | head -$i | tail -1) ./tmp.MP4; ffmpeg -i  ./tmp.MP4 -vf scale=680:-1 $(cat ./files | head -$i | tail -1); done
 
-for i in {1..$(wc files | cut -d " " -f 6)}; do echo "file $(cat ./files | head -$i | tail -1)" >> ./vidlist.txt; done
+for i in {1..$(wc files | cut -d " " -f 7)}; do echo "file $(cat ./files | head -$i | tail -1)" >> ./vidlist.txt; done
 
 ffmpeg -f concat -safe 0 -i vidlist.txt -c copy output.mp4
 
